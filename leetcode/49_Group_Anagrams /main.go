ba7b3b6ada25e0da4b0580e64079db3d00c479e5
i.demchuk <i.demchuk@bi.zone>
2025-03-07 12:48:40 +0300

    better groupAnagrams

--- leetcode/49_Group_Anagrams /main.go
+++ leetcode/49_Group_Anagrams /main.go
@@ -1,31 +1,45 @@
 // https://leetcode.com/problems/group-anagrams
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
 func main() {
-	fmt.Println(groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}))
+	//fmt.Print(groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}))
 }
+
+//func groupAnagrams(strs []string) [][]string {
+//	m := make(map[string][]string)
+//	for _, str := range strs {
+//		sortedStr := sortString(str)
+//		m[sortedStr] = append(m[sortedStr], str)
+//	}
+//
+//	var res [][]string
+//	for _, v := range m {
+//		res = append(res, v)
+//	}
+//	return res
+//}
+//
+//func sortString(s string) string {
+//	r := []rune(s)
+//	sort.Slice(r, func(i, j int) bool { return r[i] < r[j] })
+//	return string(r)
+//}
 
 func groupAnagrams(strs []string) [][]string {
-	m := make(map[string][]string)
+	m := make(map[[26]int][]string)
+
 	for _, str := range strs {
-		sortedStr := sortString(str)
-		m[sortedStr] = append(m[sortedStr], str)
+		curWordArr := [26]int{}
+		for _, c := range str {
+			curWordArr[c-'a']++
+		}
+
+		m[curWordArr] = append(m[curWordArr], str)
 	}
 
-	var res [][]string
-	for _, v := range m {
-		res = append(res, v)
+	var result [][]string
+	for _, group := range m {
+		result = append(result, group)
 	}
-	return res
-}
-
-func sortString(s string) string {
-	r := []rune(s)
-	sort.Slice(r, func(i, j int) bool { return r[i] < r[j] })
-	return string(r)
+	return result
 }
