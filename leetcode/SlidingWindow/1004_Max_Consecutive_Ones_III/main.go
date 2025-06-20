f9b1cb71ea4528c03902a9819b99da0311c7ba12
i.demchuk <i.demchuk@bi.zone>
2025-06-20 21:18:06 +0300

    1004. Max Consecutive Ones III

---
+++ leetcode/SlidingWindow/1004_Max_Consecutive_Ones_III/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(longestOnes([]int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3))
+}
+
+func longestOnes(nums []int, k int) int {
+	begin := 0
+	zeroCnt := 0
+	maxLen := 0
+
+	for end := range len(nums) {
+		if nums[end] == 0 {
+			zeroCnt++
+		}
+
+		for zeroCnt > k {
+			if nums[begin] == 0 {
+				zeroCnt--
+			}
+			begin++
+		}
+
+		curLen := end - begin + 1
+		maxLen = max(maxLen, curLen)
+	}
+
+	return maxLen
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
