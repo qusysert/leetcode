eadabe7667cc4571a1bb082ed68e7a4a2a332c45
i.demchuk <i.demchuk@bi.zone>
2025-06-23 22:27:12 +0300

    83. Remove Duplicates from Sorted List

---
+++ leetcode/LinkedList/19_Remove_Nth_Node_From_End_of_List/main.go
@@ -0,0 +1,25 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{}
+	dummy.Next = head
+
+	fast := dummy
+	slow := dummy
+
+	for range n + 1 {
+		fast = fast.Next
+	}
+
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
