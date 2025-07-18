561597322cba3f79aa461942adf39cdb9f149107
i.demchuk <i.demchuk@bi.zone>
2025-07-18 13:02:02 +0300

    71. Simplify Path

--- leetcode/71_Simplify_Path/main.go
+++ leetcode/71_Simplify_Path/main.go
@@ -3,50 +3,18 @@ package main
 import "strings"
 
 func simplifyPath(path string) string {
-	stack := New[string]()
-	els := strings.Split(path, "/")
-	for i := range len(els) {
-		if els[i] == ".." {
-			if !stack.IsEmpty() {
-				_ = stack.Pop()
-			}
+	var parts []string
+	for _, el := range strings.Split(path, "/") {
+		switch el {
+		case "", ".":
 			continue
-		}
-		if els[i] != "" && els[i] != "." {
-			stack.Push(els[i])
+		case "..":
+			if len(parts) > 0 {
+				parts = parts[:len(parts)-1]
+			}
+		default:
+			parts = append(parts, el)
 		}
 	}
-	return "/" + strings.Join(stack.GetData(), "/")
-
-}
-
-type Stack[T any] struct {
-	data []T
-}
-
-// New создает новый стек
-func New[T any]() *Stack[T] {
-	return &Stack[T]{}
-}
-
-// Push добавляет элемент в стек
-func (s *Stack[T]) Push(value T) {
-	s.data = append(s.data, value)
-}
-
-// Pop извлекает верхний элемент (предполагается, что стек не пустой)
-func (s *Stack[T]) Pop() T {
-	index := len(s.data) - 1
-	value := s.data[index]
-	s.data = s.data[:index]
-	return value
-}
-
-// IsEmpty проверяет, пуст ли стек
-func (s *Stack[T]) IsEmpty() bool {
-	return len(s.data) == 0
-}
-
-func (s *Stack[T]) GetData() []T {
-	return s.data
+	return "/" + strings.Join(parts, "/")
 }
