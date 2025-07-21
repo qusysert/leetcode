6f999e49894b52c98e025bb90429ab9f05ab5a4b
i.demchuk <i.demchuk@bi.zone>
2025-07-22 00:10:10 +0300

    98. Validate Binary Search Tree

--- leetcode/BinaryTrees/98_Validate_Binary_Search_Tree/main.go
+++ leetcode/BinaryTrees/98_Validate_Binary_Search_Tree/main.go
@@ -2,6 +2,21 @@ package main
 
 import "math"
 
+//func isValidBST(root *TreeNode) bool {
+//	return validate( root, math.MinInt, math.MaxInt)
+//}
+//func validate(node *TreeNode, lower int, upper int)bool{
+//	if node == nil{
+//		return true
+//	}
+//	if (lower < node.Val) && (node.Val < upper) {
+//		return validate( node.Left, lower, node.Val ) && validate( node.Right, node.Val, upper )
+//	} else {
+//		return false
+//	}
+//
+//}
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
