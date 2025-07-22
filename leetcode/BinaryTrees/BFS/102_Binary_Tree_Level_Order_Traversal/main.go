b589ea9d565c05d1ea97c563bac48bff14b62447
i.demchuk <i.demchuk@bi.zone>
2025-07-22 17:07:02 +0300

    102. Binary Tree Level Order Traversal

---
+++ leetcode/BinaryTrees/BFS/102_Binary_Tree_Level_Order_Traversal/main.go
@@ -0,0 +1,62 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	var res [][]int
+	q := NewQueue[*TreeNode]()
+	q.Push(root)
+
+	for !q.IsEmpty() {
+		levelSize := q.Size()
+		cur := make([]int, 0, levelSize)
+		for range levelSize {
+			node := q.Pop()
+			if node == nil {
+				continue
+			}
+			cur = append(cur, node.Val)
+			if node.Left != nil {
+				q.Push(node.Left)
+			}
+			if node.Right != nil {
+				q.Push(node.Right)
+			}
+		}
+		res = append(res, cur)
+	}
+	return res
+}
+
+type Queue[T any] struct {
+	items []T
+}
+
+func NewQueue[T any]() *Queue[T] {
+	return &Queue[T]{}
+}
+
+func (q *Queue[T]) Push(item T) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue[T]) Pop() T {
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item
+}
+
+func (q *Queue[T]) IsEmpty() bool {
+	return len(q.items) == 0
+}
+
+func (q *Queue[T]) Size() int {
+	return len(q.items)
+}
